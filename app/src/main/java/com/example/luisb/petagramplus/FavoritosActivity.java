901b8c46cc89f6a819fb8cfa083da3f979da180c
Luis Benitez <luisbenitez02@gmail.com>
2017-07-19 19:33:18 -0500

    Navegacion hacia atras
    Pusimos navegacion hacia atras en nuestro layout "Favoritos" que regresa a su padre MainActivity.
    
    Solo se modifican dos archivos!

--- app/src/main/java/com/example/luisb/petagramplus/FavoritosActivity.java
+++ app/src/main/java/com/example/luisb/petagramplus/FavoritosActivity.java
@@ -9,5 +9,8 @@ public class FavoritosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoritos);
+
+        /*vamos a activar el funcionamiento del boton "atras"*/
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
