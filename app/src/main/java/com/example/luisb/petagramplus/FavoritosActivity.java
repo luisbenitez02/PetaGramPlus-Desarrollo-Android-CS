8fa933d3b05f9b7b95a354441a236c371af79579
Luis Benitez <luisbenitez02@gmail.com>
2017-07-19 20:43:24 -0500

    Lista de favoritos (Reutilizamos Adapter y CardView)
    Reutilizamos nuestro Adaptador y CardViews creados para la lista general de mascotas.
    
    En nuestro archivo favoritos.java solo cambiamos el id del RecyclerView sobre el cual ibamos a mostrar el array de mascotas (tambien creamos solamente 5 en lugar de las 10)

--- app/src/main/java/com/example/luisb/petagramplus/FavoritosActivity.java
+++ app/src/main/java/com/example/luisb/petagramplus/FavoritosActivity.java
@@ -2,8 +2,18 @@ package com.example.luisb.petagramplus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+/*
+* Reutilziaremos el adaptador y la cardView, solo crearemos nuevos elementos
+* y cambiaremos los nombres o id de los elementos de la vista*/
 public class FavoritosActivity extends AppCompatActivity {
+
+    ArrayList<Mascota> favMascotas;//crearemos solo 5 mascotas
+    private RecyclerView rvFavMascotas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +22,37 @@ public class FavoritosActivity extends AppCompatActivity {
 
         /*vamos a activar el funcionamiento del boton "atras"*/
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /*nuestro recyclerView*/
+        rvFavMascotas = (RecyclerView) findViewById(R.id.rvFavMascotas);//le paso mi nuevo recycler
+
+        //mostramos elementos del recyclerview en una lista
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        rvFavMascotas.setLayoutManager(llm);//le entrego el layout a mi lista
+
+        inicializarListaFavMascotas();
+
+        inicializarAdapter();
+    }
+
+    /*Para inicializar nuestro adaptador
+        * Tomara el menu que creamos y lo inflara en la vista*/
+    public  void inicializarAdapter(){
+        MascotaAdaptador adapter = new MascotaAdaptador(favMascotas);
+        rvFavMascotas.setAdapter(adapter);
+    }
+
+    public void inicializarListaFavMascotas(){
+
+        favMascotas = new ArrayList<Mascota>();
+
+        favMascotas.add(new Mascota("Vaquishu","6",R.drawable.pet4));
+        favMascotas.add(new Mascota("Maite","11",R.drawable.pet6));
+        favMascotas.add(new Mascota("Pirlo","9",R.drawable.pet2));
+        favMascotas.add(new Mascota("Dani","12",R.drawable.pet3));
+        favMascotas.add(new Mascota("Yaya","28",R.drawable.pet11));
+
     }
 }
